36ac67aab866466168270936d0c509bcecdf7310
Kate Fox <katefox100@gmail.com>
2021-03-05 17:50:45 +0000

    EvenClicks component complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Multiplier from "./Components/Multiplier";
 import EvenClicks from "./Components/EvenClicks";
+import CountBy from "./Components/CountBy";
+import HideMe from "./Components/HideMe";
+import MinimumLength from "./Components/MinimumLength";
 
 const App = () => (
-  <>
-    <Multiplier x={ 5 } y={ 7 } />
-    <EvenClicks />
-  </>
+  <Router> 
+    <>
+        <Route exact path="/multiplier/:x/:y" render={ ({ match }) => (
+          <Multiplier x={ match.params.x } y={ match.params.y } />
+        ) } />
+
+        <Route exact path="/even-clicks">
+          <EvenClicks />
+        </Route>
+
+        <Route exact path="/count-by/:step" render={ ({ match }) => (
+          <CountBy step={ match.params.step } />
+        ) } />
+
+        <Route exact path="/hide-me">
+          <HideMe>Blah blah blah</HideMe>
+        </Route>
+
+        <Route exact path="/minimum/:length" render={ ({ match }) => (
+          <MinimumLength length={ match.params.length } />
+        ) } />
+    </>
+  </Router> 
 );
 
 export default App;
--- src/Components/EvenClicks.js
+++ src/Components/EvenClicks.js
@@ -1,6 +1,7 @@
 // Create a component <EvenClicks> that displays "Even" if it's been clicked an even number of times and "Odd" if it's been clicked an odd number of times.
 
 import { Component } from "react";
+import { Button } from "react-bootstrap";
 
 class EvenClicks extends Component {
 
@@ -17,7 +18,6 @@ class EvenClicks extends Component {
     handleClick() {
         let { clickedEven } = this.state;
         this.setState({ clickedEven: !clickedEven});
-        console.log("thing");
     }
 
     render() {
@@ -26,7 +26,7 @@ class EvenClicks extends Component {
         return (
             <div className="form-group">
                 <h4>EvenClicks</h4>
-                <button onClick={ this.handleClick }>Click here</button>
+                <Button onClick={ this.handleClick }>Click here</Button>
                 <p className="form-text text-muted">
                     { clickedEven ? "Even" : "Odd" }
                 </p>
